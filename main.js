f755a3ee9ebb0d9a9a39e18696216e815c0cc324
Chris Fowke <Cfowke@Chriss-MacBook-Pro.local>
2017-01-09 09:20:03 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+let inventory = [
+  ['LA-LG-STICKER', 300, 'Large Sticker', 'images/la-large-sticker-256px.png', 'Show your Launch pride by plastering your laptop with these beautiful die-cut stickers.'],
+  ['LA-SM-STICKER', 200, 'Small Sticker', 'images/la-small-sticker-128px.png', 'It\'s a tiny Launch sticker. How cute!'],
+  ['LA-T-SHRIT', 50, 'T-Shirt', 'images/la-t-shirt-200px.png', '100% Cotton. Makes a great gift.']
+]
+
+// your code, here
+class Product {
+  constructor(id, quantity, name, url, desc) {
+    this.id = id;
+    this.quantity = quantity;
+    this.name = name;
+    this.url = url;
+    this.desc = desc;
+  }
+  sell = () => {
+
+  }
+  toHTML = () => {
+
+  }
+}
